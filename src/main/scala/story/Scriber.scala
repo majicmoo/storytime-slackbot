84c624ed2f271f08cf04a807310eb16b905a2b9e
majicmoo <megan@theparkes.me.uk>
2020-03-06 20:17:21 +0000

    first pass at scriber

--- src/main/scala/story/Scriber.scala
+++ src/main/scala/story/Scriber.scala
@@ -7,22 +7,70 @@ import play.api.libs.json.Reads._
 case class Node(
     id: String,
     statement: String,
-    options: List[String],
+    optionIds: List[String],
     `type`: String
 )
 
+case class StoryOption(
+    id: String,
+    item: String,
+    tasteId: Option[String],
+    touchId: Option[String],
+    smellId: Option[String],
+    lookId: Option[String],
+    listenId: Option[String]
+)
+
+case class StoryWrapper(
+    title: String,
+    nodes: List[Node],
+    options: List[StoryOption]
+)
+
 object Scriber {
-    def parse(json: JsValue): Story = {
-        val title = (json \ "title").as[String]
-        val firstNode = (json \ "nodes" \ 0).as[JsValue]
-        
-        Story(title, jsonNodeToStoryNode(firstNode))
-    } 
+  def parse(json: JsValue): Story = {
+    implicit val nodeReads = Json.reads[Node]
+    implicit val optionReads = Json.reads[StoryOption]
+    implicit val storyReads = Json.reads[StoryWrapper]
 
-    private def jsonNodeToStoryNode(jsonNode: JsValue): NormalNode = {
-        implicit val nodeReads = Json.reads[Node]
+    val story = Json.fromJson[StoryWrapper](json).get
 
-        val node = Json.fromJson[Node](jsonNode).get
-        NormalNode(node.statement, Nil)
+    val startNode = story.nodes.head
+
+    if (startNode.`type` != "Normal") {
+      throw new Error("Bad start node")
     }
+
+    Story(story.title, createNormalNode(story, startNode))
+  }
+
+  private def findNode(story: StoryWrapper, node: Node): StoryNode = {
+    node.`type` match {
+      case "Win"    => WinNode(node.statement)
+      case "Normal" => createNormalNode(story, node)
+      case "Death"  => DeathNode(node.statement)
+      case _        => ???
+    }
+  }
+
+  private def findNodeById(story: StoryWrapper, nodeId: String): Option[StoryNode] =
+    story.nodes.find(_.id == nodeId).map(findNode(story, _))
+
+  private def createNormalNode(story: StoryWrapper, node: Node): NormalNode = {
+    val stuffToDo = node.optionIds
+      .flatMap(optionId => story.options.find(_.id == optionId))
+      .map(createNounAndActions(story))
+    NormalNode(node.statement, stuffToDo)
+  }
+
+  private def createNounAndActions(story: StoryWrapper) =
+    (option: StoryOption) =>
+      NounAndActions(
+        option.item,
+        taste = option.tasteId.flatMap(findNodeById(story, _)),
+        touch = option.touchId.flatMap(findNodeById(story, _)),
+        smell = option.smellId.flatMap(findNodeById(story, _)),
+        look = option.lookId.flatMap(findNodeById(story, _)),
+        listen = option.listenId.flatMap(findNodeById(story, _))
+      )
 }
--- src/test/scala/story/ScriberSpec.scala
+++ src/test/scala/story/ScriberSpec.scala
@@ -6,17 +6,39 @@ import scala.io.Source
 import play.api.libs.json.Json
 
 class ScriberSpec extends FlatSpec with Matchers {
-    val storyJSON = Source.fromResource("story.json").getLines.mkString
-    val json = Json.parse(storyJSON)
+  val storyJSON = Source.fromResource("story.json").getLines.mkString
+  val json = Json.parse(storyJSON)
 
   "parse" should "parse title" in {
     Scriber.parse(json).title shouldEqual story.title
   }
 
-   "parse" should "parse first node" in {
+  "parse" should "parse first node statement" in {
     Scriber.parse(json).startNode.statement shouldEqual story.startNode.statement
   }
 
+  "parse" should "parse first node stuffToDo item" in {
+    Scriber.parse(json).startNode.stuffToDo.head.item shouldEqual story.startNode.stuffToDo.head.item
+  }
+
+  "parse" should "parse first node stuffToDo touch" in {
+    Scriber.parse(json).startNode.stuffToDo.head.touch shouldEqual story.startNode.stuffToDo.head.touch
+  }
+
+  "parse" should "parse first node stuffToDo smell" in {
+    Scriber.parse(json).startNode.stuffToDo.head.smell shouldEqual story.startNode.stuffToDo.head.smell
+  }
+
+  "parse" should "parse second node stuffToDo touch" in {
+    Scriber.parse(json).startNode.stuffToDo(1).touch shouldEqual story.startNode.stuffToDo(1).touch
+  }
+
+  "parse" should "parse second node stuffToDo taste" in {
+    Scriber.parse(json).startNode.stuffToDo(1).taste shouldEqual story.startNode.stuffToDo(1).taste
+  }
+
+  "parse" should "parse all" in {
+    Scriber.parse(json) shouldEqual story
+  }
 
 }
- 
