e3802767065dad0933ecd93ce37fb1473473112e
majicmoo <megan@theparkes.me.uk>
2020-03-06 23:29:02 +0000

    Updates

---
+++ precache-manifest.54e591c7e01d4981bfe2c6a396258fbd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "acec59cacbacdeb7845eb5244b3fc4aa",
+    "url": "/storytime-slackbot/index.html"
+  },
+  {
+    "revision": "36915e96f2916d10587a",
+    "url": "/storytime-slackbot/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "05e92e5f2f22de77248d",
+    "url": "/storytime-slackbot/static/js/2.6fe90d73.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/storytime-slackbot/static/js/2.6fe90d73.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36915e96f2916d10587a",
+    "url": "/storytime-slackbot/static/js/main.d2e6f166.chunk.js"
+  },
+  {
+    "revision": "8adf98bec2a7fe4baf48",
+    "url": "/storytime-slackbot/static/js/runtime-main.9e682a49.js"
+  }
+]);
