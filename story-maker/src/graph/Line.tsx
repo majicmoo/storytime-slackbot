391a70cc4f7998aed43dcdc6f67223958c73f04c
majicmoo <megan@theparkes.me.uk>
2020-03-11 23:01:29 +0000

    fix line overflow

--- story-maker/src/graph/Line.tsx
+++ story-maker/src/graph/Line.tsx
@@ -8,7 +8,7 @@ interface LineProps {
 }
 
 const Line: FunctionComponent<LineProps> = ({ x1, y1, x2, y2 }) => (
-  <svg width="500" height="500" className="line-wrapper">
+  <svg width="100%" height="100%" className="line-wrapper">
     <line x1={x1} y1={y1} x2={x2} y2={y2} className="line" stroke="black" />
   </svg>
 );
